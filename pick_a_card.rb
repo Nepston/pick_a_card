d3a82ef0aa83f19f63f3777e9959c49554d3a851
Nepston <MaratB2010@mail.ru>
2019-01-07 22:49:50 +0300

    заменил масть на символ

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,4 +1,4 @@
 values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
-suits = ["Diamonds", "Hearts", "Clubs", "Spades"]
+suits = ["\u{2660}", "\u{2663}", "\u{2665}", "\u{2666}"]
 
-puts "#{values.sample} of #{suits.sample}"
+puts "#{values.sample}#{suits.sample}"
